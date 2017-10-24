ceb77e1630e5051f28f6a3e03a4b616acc9be2c8
Unknown <jamurano@yahoo.com>
2017-10-23 20:05:30 -0600

    WolfAI

---
+++ Chicken Game/Assets/Scripts/WolfAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI : MonoBehaviour {
+	public Rigidbody enemy;
+	public Rigidbody enemy2;
+	public float moveSpeed;
+	public Transform target;
+
+	void OnTriggerStay(Collider other){
+
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered wolfs trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+		if(other.gameObject.name == "Chicken"){
+			Debug.Log("Chicken has entered wolfs trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}
+}
+
+
