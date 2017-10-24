496979901d74d1c51c76d6c4d3912b80e1d757fa
Unknown <jamurano@yahoo.com>
2017-10-23 19:46:07 -0600

    Chicken AI

---
+++ Chicken Game/Assets/Scripts/ChickenAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAI : MonoBehaviour {
+	public Rigidbody enemy;
+	public float moveSpeed;
+	public Transform target;
+
+	// Use this for initialization
+	void OnTriggerStay(Collider other){
+
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered chickens trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*-moveSpeed*Time.deltaTime);
+		}
+	}
+}
---
+++ Chicken Game/Assets/Scripts/ChickenFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenFollow : MonoBehaviour {
+
+	public Transform target;
+	public Transform myTransform;
+	public float moveSpeed = 6f;
+
+
+	void Start () {
+
+		target = GameObject.FindWithTag("Player").transform;
+		
+	}
+	
+
+	void Update () {
+
+		transform.LookAt (target);
+		transform.Translate (Vector3.forward * Time.deltaTime * moveSpeed);
+		
+	}
+}
